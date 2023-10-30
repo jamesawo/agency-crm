aaf3d108de1c6b9c9ca6649fdcfd074579320a07
James Aworo <james.aworo@outlook.com>
2023-10-30 21:50:09 +0100

    (api): add project directory structure

---
+++ crm-api/src/main/kotlin/com/james/crm/api/features/users/domain/enums/UserTypeEnum.kt
@@ -0,0 +1,13 @@
+/*
+ * @Author: james.aworo
+ * @Date: 10/30/23
+ *
+ * @Project: agent-crm
+ */
+
+package com.james.crm.api.features.users.domain.enums
+
+enum class UserTypeEnum {
+    AGENT,
+    ADMIN
+}
