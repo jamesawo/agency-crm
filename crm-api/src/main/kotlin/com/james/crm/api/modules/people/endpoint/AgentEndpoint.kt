3e040c615b603568efff00a24c30b6bd09cae6e1
James Aworo <james.aworo@outlook.com>
2023-11-07 20:33:53 +0100

    wip(endpoint): add endpoint rest controlle

--- crm-api/src/main/kotlin/com/james/crm/api/modules/people/endpoint/AgentEndpoint.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/people/endpoint/AgentEndpoint.kt
@@ -7,5 +7,26 @@
 
 package com.james.crm.api.modules.people.endpoint
 
-class AgentEndpoint {
+import com.james.crm.api.core.constant.Route
+import com.james.crm.api.modules.people.data.dto.AgentDto
+import com.james.crm.api.modules.people.data.usecase.AgentUsecaseImpl
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${Route.BASE}/agents/")
+class AgentEndpoint(
+    private val agentUsecaseImpl: AgentUsecaseImpl
+) {
+
+    @PostMapping()
+    fun create(@RequestBody agentDto: AgentDto): ResponseEntity<AgentDto> {
+
+        val result = agentUsecaseImpl.create(agentDto)
+
+        return result;
+    }
 }
