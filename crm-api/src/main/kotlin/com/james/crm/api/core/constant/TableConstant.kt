606102b8317450dc4380f20d34389f40e6046424
James Aworo <james.aworo@outlook.com>
2023-11-07 12:31:56 +0100

    update(constant): add more database table names

--- crm-api/src/main/kotlin/com/james/crm/api/core/constant/TableConstant.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/core/constant/TableConstant.kt
@@ -10,13 +10,17 @@ package com.james.crm.api.core.constant
 class TableConstant {
 
     companion object {
-        private const val TABLE_PREFIX: String  = "crm"
-        const val USER_TABLE: String = TABLE_PREFIX + "_users"
-
-        const val CONTACT_TABLE: String = TABLE_PREFIX + "_contacts"
-
-
-        const val AGENT_TABLE: String  = TABLE_PREFIX + "_agents"
-        const val AGENT_PROFILE: String  = TABLE_PREFIX + "_agents_profile"
+        private const val TABLE_PREFIX: String = "crm_"
+        const val USER_TABLE: String = TABLE_PREFIX + "users"
+        const val CONTACT_TABLE: String = TABLE_PREFIX + "contacts"
+        const val CLIENT_TABLE: String = TABLE_PREFIX + "clients"
+        const val AGENT_TABLE: String = TABLE_PREFIX + "agents"
+        const val MANAGER_TABLE: String = TABLE_PREFIX + "managers"
+        const val PROFILE_TABLE: String = TABLE_PREFIX + "profiles"
+        const val LOCATION_TABLE: String = TABLE_PREFIX + "locations"
+        const val RESOURCE_TABLE: String = TABLE_PREFIX + "resources"
+        const val TASK_TABLE: String = TABLE_PREFIX + "tasks"
+        const val TEAM_TABLE: String = TABLE_PREFIX + "teams"
+        const val BANK_ACC_TABLE: String = TABLE_PREFIX + "bank_accounts"
     }
 }
