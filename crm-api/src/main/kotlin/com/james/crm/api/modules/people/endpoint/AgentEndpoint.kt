d5f5800d203dbc775bdb463641ed8ffaf9bcb943
James Aworo <james.aworo@outlook.com>
2023-11-08 20:58:55 +0100

    refactor(agent-endpoint): inline call to usecase create method

--- crm-api/src/main/kotlin/com/james/crm/api/modules/people/endpoint/AgentEndpoint.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/people/endpoint/AgentEndpoint.kt
@@ -9,7 +9,7 @@ package com.james.crm.api.modules.people.endpoint
 
 import com.james.crm.api.core.constant.Route
 import com.james.crm.api.modules.people.data.dto.AgentDto
-import com.james.crm.api.modules.people.data.usecase.AgentUsecaseImpl
+import com.james.crm.api.modules.people.data.usecase.contract.IAgentUsecase
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,14 +19,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("${Route.BASE}/agents/")
 class AgentEndpoint(
-    private val agentUsecaseImpl: AgentUsecaseImpl
+    private val agentUsecaseImpl: IAgentUsecase
 ) {
-
     @PostMapping()
     fun create(@RequestBody agentDto: AgentDto): ResponseEntity<AgentDto> {
-
-        val result = agentUsecaseImpl.create(agentDto)
-
-        return result;
+        return agentUsecaseImpl.create(agentDto);
     }
 }
