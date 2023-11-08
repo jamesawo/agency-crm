62d0879f983592f2d9f83296a64328e0e42f1b06
James Aworo <james.aworo@outlook.com>
2023-11-08 23:48:12 +0100

    update(people): add usecase annotation for business usecases

---
+++ crm-api/src/main/kotlin/com/james/crm/api/core/annotation/Usecase.kt
@@ -0,0 +1,18 @@
+/*
+ * @Author: james.aworo
+ * @Date: 11/8/23
+ *
+ * @Project: agent-crm
+ */
+
+package com.james.crm.api.core.annotation
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class Usecase(@get:AliasFor(annotation = Component::class) val value: String = "")
