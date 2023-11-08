8cc4f670e631bd7d84c28004dfd167cb5bc35f10
James Aworo <james.aworo@outlook.com>
2023-11-08 20:57:02 +0100

    wip(people): add agent usecase implementation

--- crm-api/src/main/kotlin/com/james/crm/api/modules/people/data/usecase/implementation/AgentUsecaseImpl.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/people/data/usecase/implementation/AgentUsecaseImpl.kt
@@ -5,20 +5,38 @@
  * @Project: agent-crm
  */
 
-package com.james.crm.api.modules.people.data.usecase
+package com.james.crm.api.modules.people.data.usecase.implementation
 
 import com.james.crm.api.modules.people.data.dto.AgentDto
+import com.james.crm.api.modules.people.data.usecase.contract.IAgentUsecase
 import com.james.crm.api.modules.people.domain.repository.AgentDataRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
-class AgentUsecaseImpl(private val repository: AgentDataRepository)  {
-
-    fun create(agentDto: AgentDto): ResponseEntity<AgentDto>{
-        val agent = agentDto.toEntity()
-        println(agent.contact.email)
-        return ResponseEntity.ok(agentDto)
+class AgentUsecaseImpl(
+    private val repository: AgentDataRepository
+): IAgentUsecase  {
+    override fun create(agent: AgentDto): ResponseEntity<AgentDto> {
+        val savedAgent = repository.save(agent.toEntity())
+        return ResponseEntity.ok().body(agent.toTrimmedRequest(savedAgent))
     }
+
+    override fun find(agentId: String): ResponseEntity<AgentDto?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun find(): ResponseEntity<List<AgentDto>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findByEmail(email: String): ResponseEntity<AgentDto?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(agent: AgentDto): ResponseEntity<AgentDto> {
+        TODO("Not yet implemented")
+    }
+
 }
 
