7b8658bd6d10e80fc4f29bc612f54bd62079640c
James Aworo <james.aworo@outlook.com>
2023-11-10 10:03:19 +0100

    update(ManagerDto): implement Mapper interface

--- crm-api/src/main/kotlin/com/james/crm/api/modules/people/data/dto/ManagerDto.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/people/data/dto/ManagerDto.kt
@@ -2,24 +2,55 @@ package com.james.crm.api.modules.people.data.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.james.crm.api.core.model.Mapper
+import com.james.crm.api.modules.people.domain.model.Agent
 import com.james.crm.api.modules.people.domain.model.Manager
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 class ManagerDto {
-    val id: String? = null
+    var id: String? = null
     var profile: ProfileDto = ProfileDto()
     var contact: ContactDto = ContactDto()
     var user: UserDto = UserDto()
     var agents: MutableList<AgentDto> = mutableListOf()
 
+    constructor(
+        id: String?,
+        profile: ProfileDto,
+        contact: ContactDto,
+        user: UserDto,
+        agents: MutableList<AgentDto>
+    ) : this() {
+        this.id = id
+        this.profile = profile
+        this.contact = contact
+        this.user = user
+        this.agents = agents
+    }
+
+    constructor()
+
+
     companion object : Mapper<ManagerDto, Manager> {
         override fun toEntity(request: ManagerDto): Manager {
-            TODO("Not yet implemented")
+            val manager = Manager()
+            manager.profile = ProfileDto.toEntity(request.profile)
+            manager.contact = ContactDto.toEntity(request.contact)
+            manager.user = UserDto.toEntity(request.user)
+            manager.agents = request.agents.map { Agent(id = it.id) }
+            return manager
         }
+
 
         override fun toRequest(entity: Manager): ManagerDto {
-            TODO("Not yet implemented")
+            return ManagerDto(
+                id = entity.id,
+                profile = ProfileDto.toRequest(entity.profile),
+                contact = ContactDto.toRequest(entity.contact),
+                user = UserDto.toRequest(entity.user),
+                agents = entity.agents.map { AgentDto.toRequest(it) }.toMutableList()
+            )
         }
-
     }
+
+
 }
