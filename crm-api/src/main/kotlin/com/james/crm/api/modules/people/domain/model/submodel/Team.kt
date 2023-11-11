83b275d8384781b3ddb461cb1e2e028ad93bcb99
James Aworo <james.aworo@outlook.com>
2023-11-11 05:22:03 +0100

    update(Team): add model properties

--- crm-api/src/main/kotlin/com/james/crm/api/modules/people/domain/model/submodel/Team.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/people/domain/model/submodel/Team.kt
@@ -8,12 +8,35 @@
 package com.james.crm.api.modules.people.domain.model.submodel
 
 import com.james.crm.api.core.constant.DatabaseTable.Companion.TEAM
+import com.james.crm.api.core.constant.DatabaseTable.Companion.TEAM_TASKS
 import com.james.crm.api.core.model.Base
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import com.james.crm.api.modules.people.domain.model.Agent
+import com.james.crm.api.modules.people.domain.model.Manager
+import jakarta.persistence.*
 
 @Entity
 @Table(name = TEAM)
-class Team: Base() {
+class Team(id: String? = null) : Base(id) {
+
     var title: String = ""
+
+    @ManyToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    var manager: Manager? = Manager()
+
+    @OneToMany(mappedBy = "team", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    var agents: List<Agent>? = emptyList()
+
+    @OneToOne(cascade = [CascadeType.ALL])
+    var location: Location? = Location()
+
+    var budget: Double = 0.00
+
+    @ManyToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    @JoinTable(
+        name = TEAM_TASKS,
+        joinColumns = [JoinColumn(name = "team_id")],
+        inverseJoinColumns = [JoinColumn(name = "task_id")]
+    )
+    var tasks: List<Task>? = mutableListOf()
+
 }
