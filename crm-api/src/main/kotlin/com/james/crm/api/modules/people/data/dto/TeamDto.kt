be74812457906b1cd8e48454c45ec57d49936233
James Aworo <james.aworo@outlook.com>
2023-11-11 05:21:33 +0100

    add team data transfer object class with mapper implementations

--- crm-api/src/main/kotlin/com/james/crm/api/modules/people/data/dto/TeamDto.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/people/data/dto/TeamDto.kt
@@ -1,6 +1,69 @@
 package com.james.crm.api.modules.people.data.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.james.crm.api.core.model.Mapper
+import com.james.crm.api.modules.people.domain.model.submodel.Team
+
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-class TeamDto
+data class TeamDto(var id: String?) {
+    var title: String = ""
+    var manager: ManagerDto? = null
+    var agents: List<AgentDto>? = null
+    var location: LocationDto? = null
+    var budget: Double = 0.00
+    var tasks: List<TaskDto>? = null
+
+    constructor() : this(id = null)
+
+    constructor(
+        id: String?,
+        title: String,
+        manager: ManagerDto?,
+        agents: List<AgentDto>?,
+        location: LocationDto?,
+        budget: Double,
+        tasks: List<TaskDto>?
+    ) : this(id) {
+        this.title = title
+        this.manager = manager
+        this.agents = agents
+        this.location = location
+        this.budget = budget
+        this.tasks = tasks
+    }
+
+    companion object : Mapper<TeamDto, Team> {
+
+        override fun toRequest(entity: Team): TeamDto {
+            return TeamDto(
+                id = entity.id,
+                title = entity.title,
+                manager = entity.manager?.let { ManagerDto.toRequest(it) },
+                agents = entity.agents?.map { AgentDto.toRequest(it) },
+                location = entity.location?.let { LocationDto.toRequest(it) },
+                budget = entity.budget,
+                tasks = entity.tasks?.map { TaskDto.toRequest(it) }
+            )
+        }
+
+        override fun toEntity(request: TeamDto): Team {
+            val team = Team()
+            team.title = request.title
+            team.manager = request.manager?.let { ManagerDto.toEntity(it) }
+            team.agents = request.agents?.map { AgentDto.toEntity(it) }
+            team.location = request.location?.let { LocationDto.toEntity(it) }
+            team.budget = request.budget
+            team.tasks = request.tasks?.map { TaskDto.toEntity(it) }
+            return team
+        }
+
+        override fun toTrimmedRequest(entity: Team): TeamDto {
+            return toRequest(entity).apply {
+                agents = null
+                tasks = null
+                manager = null
+            }
+        }
+    }
+}
