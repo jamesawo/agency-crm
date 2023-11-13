13950463de26866378e63b20cebe948906951bf5
James Aworo <james.aworo@outlook.com>
2023-11-13 16:12:30 +0100

    feature: set team location

--- crm-api/src/main/kotlin/com/james/crm/api/modules/team/data/usecase/implementation/SetTeamLocationUsecase.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/team/data/usecase/implementation/SetTeamLocationUsecase.kt
@@ -9,15 +9,32 @@ package com.james.crm.api.modules.team.data.usecase.implementation
 
 import com.james.crm.api.core.annotation.Usecase
 import com.james.crm.api.core.common.ApiResponse
+import com.james.crm.api.core.common.CatchableError
+import com.james.crm.api.core.util.Util
+import com.james.crm.api.core.util.Util.Companion.errorResponse
+import com.james.crm.api.core.util.Util.Companion.successResponse
 import com.james.crm.api.modules.people.data.dto.LocationDto
-import com.james.crm.api.modules.team.data.dto.TeamDetailDto
+import com.james.crm.api.modules.team.data.dto.TeamLocationDto
+import com.james.crm.api.modules.team.data.repository.TeamDataRepository
 import com.james.crm.api.modules.team.data.usecase.contract.ISetTeamLocationUsecase
+import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 
 @Usecase
-class SetTeamLocationUsecase : ISetTeamLocationUsecase {
+class SetTeamLocationUsecase(
+    private val teamRepository: TeamDataRepository
+) : ISetTeamLocationUsecase {
 
-    override fun execute(input: Pair<String, LocationDto>): ResponseEntity<ApiResponse<TeamDetailDto>> {
-        TODO("Not yet implemented")
+    override fun execute(input: Pair<String, LocationDto>): ResponseEntity<ApiResponse<TeamLocationDto>> {
+        return try {
+            teamRepository.findById(input.first).map { team ->
+                team.location = LocationDto.toEntity(input.second)
+                val save = teamRepository.save(team)
+                successResponse(OK, TeamLocationDto.toTrimRequest(save))
+            }.orElse(errorResponse(NOT_FOUND, Util.notFoundMessageAsList("team")))
+
+        } catch (ex: Exception) {
+            errorResponse(INTERNAL_SERVER_ERROR, CatchableError(INTERNAL_SERVER_ERROR, listOf(ex.localizedMessage), ex))
+        }
     }
 }
