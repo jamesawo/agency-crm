3495462e839a46d94dba7825bd11be1e65a5ffd3
James Aworo <james.aworo@outlook.com>
2023-11-13 15:40:56 +0100

    refactor(Team): restructure team usecase contracts and implementations

--- crm-api/src/main/kotlin/com/james/crm/api/modules/team/endpoint/TeamEndpoint.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/team/endpoint/TeamEndpoint.kt
@@ -9,13 +9,16 @@ package com.james.crm.api.modules.team.endpoint
 
 import com.james.crm.api.core.common.ApiResponse
 import com.james.crm.api.core.common.Empty
+import com.james.crm.api.core.common.Paginate
 import com.james.crm.api.core.constant.Route
-import com.james.crm.api.modules.people.data.dto.LocationDto
 import com.james.crm.api.modules.team.data.dto.TeamDetailDto
 import com.james.crm.api.modules.team.data.dto.TeamDto
+import com.james.crm.api.modules.team.data.dto.TeamLocationDto
 import com.james.crm.api.modules.team.data.usecase.contract.*
 import com.james.crm.api.modules.team.data.usecase.implementation.GetTeamDetailUsecaseImpl
 import jakarta.validation.Valid
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -26,8 +29,18 @@ class TeamEndpoint(
     private var getLocationUsecase: IGetAllTeamLocationUsecase,
     private var setTeamBudgetUsecase: ISetTeamBudgetUseCase,
     private var setTeamTaskUseCase: ISetTeamTaskUseCase,
-    private var getTeamDetailUsecaseImpl: GetTeamDetailUsecaseImpl
+    private var getTeamDetailUsecaseImpl: GetTeamDetailUsecaseImpl,
+    private var getTeamsUsecase: IGetTeamsUsecase
 ) {
+
+    @GetMapping("/all")
+    fun getAllTeams(
+        @RequestParam(defaultValue = "1") page: Int,
+        @RequestParam(defaultValue = "10") size: Int,
+        @RequestParam(defaultValue = "ASC", required = false) sort: Sort.Direction
+    ): ResponseEntity<ApiResponse<Paginate<TeamDetailDto>>> {
+        return getTeamsUsecase.execute(PageRequest.of(page - 1, size, Sort.by(sort, "title")))
+    }
 
     @PostMapping
     fun createTeam(
@@ -37,7 +50,7 @@ class TeamEndpoint(
     }
 
     @GetMapping("/all-locations")
-    fun getTeamLocation(): ResponseEntity<ApiResponse<List<LocationDto>>> {
+    fun getTeamLocation(): ResponseEntity<ApiResponse<List<TeamLocationDto>>> {
         return getLocationUsecase.execute(Empty())
     }
 
