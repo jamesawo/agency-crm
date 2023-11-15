2c5d8a6a5b99721ba87d48f471b46f74d29d9be6
James Aworo <james.aworo@outlook.com>
2023-11-16 00:01:22 +0100

    update(pipeline): add endpoint routes

--- crm-api/src/main/kotlin/com/james/crm/api/modules/pipeline/endpoint/PipelineEndpoint.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/pipeline/endpoint/PipelineEndpoint.kt
@@ -7,13 +7,19 @@
 
 package com.james.crm.api.modules.pipeline.endpoint
 
+import com.james.crm.api.core.common.ApiResponse
+import com.james.crm.api.core.common.Paginate
 import com.james.crm.api.core.constant.Route
+import com.james.crm.api.modules.pipeline.data.dto.PipelineDto
 import com.james.crm.api.modules.pipeline.data.usecase.contract.pipeline.ICreatePipelineUsecase
 import com.james.crm.api.modules.pipeline.data.usecase.contract.pipeline.IGetAllPipelineUsecase
 import com.james.crm.api.modules.pipeline.data.usecase.contract.pipeline.IGetPipelineUsecase
 import com.james.crm.api.modules.pipeline.data.usecase.contract.pipeline.IUpdatePipelineUsecase
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("${Route.API_VERSION}/pipelines")
@@ -22,5 +28,39 @@ class PipelineEndpoint(
     private val getAllPipelineUsecase: IGetAllPipelineUsecase,
     private val getPipelineUsecase: IGetPipelineUsecase,
     private val updatePipelineUsecase: IUpdatePipelineUsecase
-    
-)
+
+) {
+    @PostMapping
+    fun createPipeline(
+        @Valid @RequestBody pipelineDto: PipelineDto
+    ): ResponseEntity<ApiResponse<PipelineDto>> {
+        return createPipelineUsecase.execute(pipelineDto)
+    }
+
+    @GetMapping("/all")
+    fun getAllPipeline(
+        @RequestParam(defaultValue = "1") page: Int,
+        @RequestParam(defaultValue = "10") size: Int,
+        @RequestParam(defaultValue = "ASC", required = false) sort: Sort.Direction
+    ): ResponseEntity<ApiResponse<Paginate<PipelineDto>>> {
+        return getAllPipelineUsecase.execute(
+            PageRequest.of(
+                page - 1, size,
+                Sort.by(sort, "hierarchy")
+            )
+        )
+    }
+
+    @GetMapping("/{pipelineId}")
+    fun getOnePipeline(@PathVariable pipelineId: String): ResponseEntity<ApiResponse<PipelineDto>> {
+        return getPipelineUsecase.execute(pipelineId)
+    }
+
+    @PutMapping("{pipelineId}")
+    fun updatePipeline(
+        @PathVariable pipelineId: String,
+        @Valid @RequestBody pipelineDto: PipelineDto
+    ): ResponseEntity<ApiResponse<Boolean>> {
+        return updatePipelineUsecase.execute(Pair(pipelineId, pipelineDto))
+    }
+}
--- crm-api/src/main/kotlin/com/james/crm/api/modules/pipeline/endpoint/StageEndpoint.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/pipeline/endpoint/StageEndpoint.kt
@@ -7,13 +7,16 @@
 
 package com.james.crm.api.modules.pipeline.endpoint
 
+import com.james.crm.api.core.common.ApiResponse
 import com.james.crm.api.core.constant.Route
+import com.james.crm.api.modules.pipeline.data.dto.StageDto
 import com.james.crm.api.modules.pipeline.data.usecase.contract.stage.IAddStagesUsecase
 import com.james.crm.api.modules.pipeline.data.usecase.contract.stage.IGetAllStagesUsecase
 import com.james.crm.api.modules.pipeline.data.usecase.contract.stage.IGetStageUsecase
 import com.james.crm.api.modules.pipeline.data.usecase.contract.stage.IUpdateStageUsecase
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("${Route.API_VERSION}/stages")
@@ -22,4 +25,34 @@ class StageEndpoint(
     private val getAllStageUsecase: IGetAllStagesUsecase,
     private val getStageUsecase: IGetStageUsecase,
     private val updateStageUsecase: IUpdateStageUsecase
-)
+) {
+    @PutMapping("/{pipelineId}")
+    fun addStageToPipeline(
+        @PathVariable pipelineId: String,
+        @RequestBody stageDto: StageDto
+    ): ResponseEntity<ApiResponse<StageDto>> {
+        return addStageUsecase.execute(Pair(pipelineId, stageDto))
+    }
+
+    @GetMapping("/{pipelineId}")
+    fun getAllStageInPipeline(
+        @PathVariable pipelineId: String
+    ): ResponseEntity<ApiResponse<List<StageDto>>> {
+        return getAllStageUsecase.execute(pipelineId)
+    }
+
+    @GetMapping("/{stageId}")
+    fun getOneStageById(
+        @PathVariable stageId: String
+    ): ResponseEntity<ApiResponse<StageDto>> {
+        return getStageUsecase.execute(stageId)
+    }
+
+    @PutMapping("/{stageId}")
+    fun updateOneStage(
+        @PathVariable stageId: String,
+        @Valid @RequestBody stageDto: StageDto
+    ): ResponseEntity<ApiResponse<Boolean>> {
+        return updateStageUsecase.execute(Pair(stageId, stageDto))
+    }
+}
