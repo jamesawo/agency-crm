9bd10d67184a769822c7cad127b9f897385bc039
James Aworo <james.aworo@outlook.com>
2023-11-15 18:58:08 +0100

    update(PipelineDto): add new property called hierarchy and new secondary constructor

--- crm-api/src/main/kotlin/com/james/crm/api/modules/pipeline/data/dto/PipelineDto.kt
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/pipeline/data/dto/PipelineDto.kt
@@ -9,10 +9,20 @@ package com.james.crm.api.modules.pipeline.data.dto
 
 import com.james.crm.api.core.common.Mapper
 import com.james.crm.api.modules.pipeline.domain.Pipeline
-import com.james.crm.api.modules.pipeline.domain.Stage
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
-data class PipelineDto(var id: String? = null) {
-    var title: String = ""
+data class PipelineDto(
+    var id: String? = null,
+
+    @NotBlank
+    var title: String = "",
+
+    @NotNull
+    var hierarchy: Int = 0
+) {
+
+
     var stages: List<StageDto> = emptyList()
 
     constructor() : this(id = null)
@@ -20,10 +30,12 @@ data class PipelineDto(var id: String? = null) {
     constructor(
         id: String?,
         title: String,
+        hierarchy: Int,
         stages: List<StageDto>
     ) : this(id = null) {
         this.id = id
         this.title = title
+        this.hierarchy = hierarchy
         this.stages = stages
     }
 
@@ -32,12 +44,26 @@ data class PipelineDto(var id: String? = null) {
             return Pipeline(
                 id = request.id,
                 title = request.title,
-                stages = request.stages.map { Stage(it.id) }
+                hierarchy = request.hierarchy,
             )
+            //stages = request.stages.map { Stage(it.id) }
         }
 
         override fun toRequest(entity: Pipeline): PipelineDto {
-            TODO("Not yet implemented")
+            return PipelineDto(
+                id = entity.id,
+                title = entity.title,
+                hierarchy = entity.hierarchy,
+                stages = entity.stages.map { StageDto(id = it.id, title = it.title) }
+            )
+        }
+
+        override fun toTrimRequest(entity: Pipeline): PipelineDto {
+            return PipelineDto(
+                id = entity.id,
+                title = entity.title,
+                hierarchy = entity.hierarchy,
+            )
         }
     }
 }
