bbc93ceb73f6045c9a34f9a9899ef61e3f2463dd
James Aworo <james.aworo@outlook.com>
2023-11-16 23:04:49 +0100

    add task domain classes

---
+++ crm-api/src/main/kotlin/com/james/crm/api/modules/task/domain/enums/TaskStatus.kt
@@ -0,0 +1,16 @@
+/*
+ * @Author: james.aworo
+ * @Date: 11/16/23
+ *
+ * @Project: agent-crm
+ */
+
+package com.james.crm.api.modules.task.domain.enums
+
+
+enum class TaskStatus {
+    OPEN,
+    CLOSED,
+    PENDING,
+    COMPLETED
+}
