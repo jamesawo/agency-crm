95bf4cbc7fca0f396319c2eaa01ba82b07e329e7
James Aworo <james.aworo@outlook.com>
2023-11-21 23:20:31 +0100

    update(crm-web): add lazy module routing for application modules

--- crm-web/src/app/pages/pages-routing.module.ts
+++ crm-web/src/app/pages/pages-routing.module.ts
@@ -3,15 +3,43 @@ import {NgModule} from '@angular/core';
 
 import {PagesComponent} from './pages.component';
 import {NotFoundComponent} from './miscellaneous/not-found/not-found.component';
-import {WelcomeComponent} from './welcome/welcome/welcome.component';
+import {WelcomeComponent} from './miscellaneous/welcome/welcome.component';
 
 const routes: Routes = [{
     path: '',
     component: PagesComponent,
     children: [
         {
-            path: 'welcome',
+            path: '',
             component: WelcomeComponent,
+        },
+        {
+            path: 'dashboard',
+            loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+        },
+        {
+            path: 'pipeline',
+            loadChildren: () => import('./pipeline/pipeline-routing.module').then(m => m.PipelineRoutingModule),
+        },
+        {
+            path: 'task',
+            loadChildren: () => import('./task/task.module').then(m => m.TaskModule),
+        },
+        {
+            path: 'team',
+            loadChildren: () => import('./team/team.module').then(m => m.TeamModule),
+        },
+        {
+            path: 'supervisor',
+            loadChildren: () => import('./supervisor/supervisor.module').then(m => m.SupervisorModule),
+        },
+        {
+            path: 'agent',
+            loadChildren: () => import('./agent/agent.module').then(m => m.AgentModule),
+        },
+        {
+            path: 'authorization',
+            loadChildren: () => import('./authorization/authorization.module').then(m => m.AuthorizationModule),
         },
         {
             path: '**',
