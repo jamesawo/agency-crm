af4305d9f4313512dcf49c78f4ed3e2b8c1a25e8
James Aworo <james.aworo@outlook.com>
2023-11-22 23:06:53 +0100

    update(crm-web): clean up miscellaneous module

--- crm-web/src/app/pages/miscellaneous/miscellaneous.module.ts
+++ crm-web/src/app/pages/miscellaneous/miscellaneous.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
-import { NbButtonModule, NbCardModule } from '@nebular/theme';
-
-import { ThemeModule } from '../../@theme/theme.module';
-import { MiscellaneousRoutingModule } from './miscellaneous-routing.module';
-import { MiscellaneousComponent } from './miscellaneous.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import {NgModule} from '@angular/core';
+import {SharedModule} from '../shared/shared.module';
+import {MiscellaneousRoutingModule} from './miscellaneous-routing.module';
+import {MiscellaneousComponent} from './miscellaneous.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 @NgModule({
-  imports: [
-    ThemeModule,
-    NbCardModule,
-    NbButtonModule,
-    MiscellaneousRoutingModule,
-  ],
-  declarations: [
-    MiscellaneousComponent,
-    NotFoundComponent,
-  ],
+    imports: [
+        SharedModule,
+        MiscellaneousRoutingModule,
+    ],
+    declarations: [
+        MiscellaneousComponent,
+        NotFoundComponent,
+    ],
 })
-export class MiscellaneousModule { }
+export class MiscellaneousModule {
+}
--- crm-web/src/app/pages/miscellaneous/not-found/not-found.component.ts
+++ crm-web/src/app/pages/miscellaneous/not-found/not-found.component.ts
@@ -1,17 +1,17 @@
-import { NbMenuService } from '@nebular/theme';
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NbMenuService} from '@nebular/theme';
 
 @Component({
-  selector: 'ngx-not-found',
-  styleUrls: ['./not-found.component.scss'],
-  templateUrl: './not-found.component.html',
+    selector: 'ngx-not-found',
+    styleUrls: ['./not-found.component.scss'],
+    templateUrl: './not-found.component.html',
 })
 export class NotFoundComponent {
 
-  constructor(private menuService: NbMenuService) {
-  }
+    constructor(private menuService: NbMenuService) {
+    }
 
-  goToHome() {
-    this.menuService.navigateHome();
-  }
+    goToHome() {
+        this.menuService.navigateHome();
+    }
 }
