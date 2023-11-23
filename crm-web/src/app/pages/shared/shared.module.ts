69b3aa7ed37f586d48fb8ed0ec7f7e36909943c2
James Aworo <james.aworo@outlook.com>
2023-11-23 20:33:50 +0100

    refactor(crm-web): update shared module to use const variable for module imports

--- crm-web/src/app/pages/shared/shared.module.ts
+++ crm-web/src/app/pages/shared/shared.module.ts
@@ -1,20 +1,28 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {PageSpacerComponent} from './components/page-spacer/page-spacer.component';
 import {SHARED_COMPONENTS} from './shared-components';
 import {SHARED_MODULES} from './shared-module';
 import {SHARED_NEBULA_MODULES} from './shared-nebula-module';
+import {SHARED_THIRD_MODULES} from './shared-third-module';
 
 @NgModule({
-    declarations: [...SHARED_COMPONENTS],
+    declarations: [
+        ...SHARED_COMPONENTS,
+        PageSpacerComponent,
+    ],
     imports: [
         CommonModule,
         ...SHARED_MODULES,
         ...SHARED_NEBULA_MODULES,
+        ...SHARED_THIRD_MODULES,
     ],
     exports: [
         ...SHARED_COMPONENTS,
         ...SHARED_MODULES,
         ...SHARED_NEBULA_MODULES,
+        ...SHARED_THIRD_MODULES,
+        PageSpacerComponent,
     ],
 })
 export class SharedModule {
