e8f080d0ddc4f09ae570d60e84bd68b95de86e19
James Aworo <james.aworo@outlook.com>
2023-11-23 20:31:43 +0100

    refactor(crm-web): restructure pipeline imports into a separate const variable

--- crm-web/src/app/pages/pipeline/pipeline.module.ts
+++ crm-web/src/app/pages/pipeline/pipeline.module.ts
@@ -1,19 +1,30 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Type} from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
+import {PipelineFilterComponent} from './_components/pipeline-filter/pipeline-filter.component';
+import {PipelineTableComponent} from './_components/pipeline-table/pipeline-table.component';
 
 import {PipelineRoutingModule} from './pipeline-routing.module';
 import {PipelinesComponent} from './pipelines/pipelines.component';
 import {StagesComponent} from './stages/stages.component';
 
+const COMPONENTS: Array<Type<any>> = [
+    StagesComponent,
+    PipelinesComponent,
+    PipelineFilterComponent,
+    PipelineTableComponent,
+];
 
 @NgModule({
     declarations: [
-        StagesComponent,
-        PipelinesComponent,
+        ...COMPONENTS,
     ],
     imports: [
         SharedModule,
         PipelineRoutingModule,
+    ],
+    exports: [
+        SharedModule,
+        ...COMPONENTS,
     ],
 })
 export class PipelineModule {
