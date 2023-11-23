6bb33d8b80dac29584352ac506a42adcd106da6a
James Aworo <james.aworo@outlook.com>
2023-11-23 20:28:32 +0100

    refactor(crm-web): remove unnecessary service class

--- crm-web/src/app/@core/core.module.ts
+++ crm-web/src/app/@core/core.module.ts
@@ -1,51 +1,51 @@
-import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
 import {NbAuthModule, NbDummyAuthStrategy} from '@nebular/auth';
 import {NbRoleProvider, NbSecurityModule} from '@nebular/security';
 import {of as observableOf} from 'rxjs';
-
-import {throwIfAlreadyLoaded} from './module-import-guard';
-import {AnalyticsService, LayoutService, PlayerService, SeoService, StateService} from './utils';
-import {UserData} from './data/users';
-import {ElectricityData} from './data/electricity';
-import {SmartTableData} from './data/smart-table';
-import {UserActivityData} from './data/user-activity';
-import {OrdersChartData} from './data/orders-chart';
-import {ProfitChartData} from './data/profit-chart';
-import {TrafficListData} from './data/traffic-list';
-import {EarningData} from './data/earning';
-import {OrdersProfitChartData} from './data/orders-profit-chart';
-import {TrafficBarData} from './data/traffic-bar';
-import {ProfitBarAnimationChartData} from './data/profit-bar-animation-chart';
-import {TemperatureHumidityData} from './data/temperature-humidity';
-import {SolarData} from './data/solar';
-import {TrafficChartData} from './data/traffic-chart';
-import {StatsBarData} from './data/stats-bar';
 import {CountryOrderData} from './data/country-order';
-import {StatsProgressBarData} from './data/stats-progress-bar';
-import {VisitorsAnalyticsData} from './data/visitors-analytics';
+import {EarningData} from './data/earning';
+import {ElectricityData} from './data/electricity';
+import {OrdersChartData} from './data/orders-chart';
+import {OrdersProfitChartData} from './data/orders-profit-chart';
+import {ProfitBarAnimationChartData} from './data/profit-bar-animation-chart';
+import {ProfitChartData} from './data/profit-chart';
 import {SecurityCamerasData} from './data/security-cameras';
+import {SmartTableData} from './data/smart-table';
+import {SolarData} from './data/solar';
+import {StatsBarData} from './data/stats-bar';
+import {StatsProgressBarData} from './data/stats-progress-bar';
+import {TemperatureHumidityData} from './data/temperature-humidity';
+import {TrafficBarData} from './data/traffic-bar';
+import {TrafficChartData} from './data/traffic-chart';
+import {TrafficListData} from './data/traffic-list';
+import {UserActivityData} from './data/user-activity';
+import {UserData} from './data/users';
+import {VisitorsAnalyticsData} from './data/visitors-analytics';
+import {CountryOrderService} from './mock/country-order.service';
+import {EarningService} from './mock/earning.service';
+import {ElectricityService} from './mock/electricity.service';
+import {MockDataModule} from './mock/mock-data.module';
+import {OrdersChartService} from './mock/orders-chart.service';
+import {OrdersProfitChartService} from './mock/orders-profit-chart.service';
+import {ProfitBarAnimationChartService} from './mock/profit-bar-animation-chart.service';
+import {ProfitChartService} from './mock/profit-chart.service';
+import {SecurityCamerasService} from './mock/security-cameras.service';
+import {SmartTableService} from './mock/smart-table.service';
+import {SolarService} from './mock/solar.service';
+import {StatsBarService} from './mock/stats-bar.service';
+import {StatsProgressBarService} from './mock/stats-progress-bar.service';
+import {TemperatureHumidityService} from './mock/temperature-humidity.service';
+import {TrafficBarService} from './mock/traffic-bar.service';
+import {TrafficChartService} from './mock/traffic-chart.service';
+import {TrafficListService} from './mock/traffic-list.service';
+import {UserActivityService} from './mock/user-activity.service';
 
 import {UserService} from './mock/users.service';
-import {ElectricityService} from './mock/electricity.service';
-import {SmartTableService} from './mock/smart-table.service';
-import {UserActivityService} from './mock/user-activity.service';
-import {OrdersChartService} from './mock/orders-chart.service';
-import {ProfitChartService} from './mock/profit-chart.service';
-import {TrafficListService} from './mock/traffic-list.service';
-import {EarningService} from './mock/earning.service';
-import {OrdersProfitChartService} from './mock/orders-profit-chart.service';
-import {TrafficBarService} from './mock/traffic-bar.service';
-import {ProfitBarAnimationChartService} from './mock/profit-bar-animation-chart.service';
-import {TemperatureHumidityService} from './mock/temperature-humidity.service';
-import {SolarService} from './mock/solar.service';
-import {TrafficChartService} from './mock/traffic-chart.service';
-import {StatsBarService} from './mock/stats-bar.service';
-import {CountryOrderService} from './mock/country-order.service';
-import {StatsProgressBarService} from './mock/stats-progress-bar.service';
 import {VisitorsAnalyticsService} from './mock/visitors-analytics.service';
-import {SecurityCamerasService} from './mock/security-cameras.service';
-import {MockDataModule} from './mock/mock-data.module';
+
+import {throwIfAlreadyLoaded} from './module-import-guard';
+import {AnalyticsService, LayoutService, SeoService, StateService} from './utils';
 
 const socialLinks = [
     {
@@ -134,7 +134,6 @@ export const NB_CORE_PROVIDERS = [
     },
     AnalyticsService,
     LayoutService,
-    PlayerService,
     SeoService,
     StateService,
 ];
