00189f2279408fbc65ab7e205e559b8f65e5878b
James Aworo <james.aworo@outlook.com>
2023-11-24 19:28:53 +0100

    update(crm-web): add new shared components in separate shared components for clean import

--- crm-web/src/app/pages/shared/shared-components.ts
+++ crm-web/src/app/pages/shared/shared-components.ts
@@ -1,7 +1,11 @@
 import {Type} from '@angular/core';
 import {PageButtonsComponent} from './components/page-buttons/page-buttons.component';
 import {PageHeaderComponent} from './components/page-header/page-header.component';
+import {PageIconButtonComponent} from './components/page-icon-button/page-icon-button.component';
 import {PageSearchFieldsComponent} from './components/page-search-fields/page-search-fields.component';
+import {PageSpacerComponent} from './components/page-spacer/page-spacer.component';
+import {PageTableHeaderComponent} from './components/page-table-header/page-table-header.component';
+import {PageTableComponent} from './components/page-table/page-table.component';
 import {PageViewButtonsComponent} from './components/page-view-buttons/page-view-buttons.component';
 
 export const SHARED_COMPONENTS: Array<Type<void>> = [
@@ -9,4 +13,8 @@ export const SHARED_COMPONENTS: Array<Type<void>> = [
     PageButtonsComponent,
     PageViewButtonsComponent,
     PageSearchFieldsComponent,
+    PageSpacerComponent,
+    PageIconButtonComponent,
+    PageTableComponent,
+    PageTableHeaderComponent,
 ];
