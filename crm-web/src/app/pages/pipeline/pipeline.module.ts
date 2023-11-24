9870a6ed465f4aae9683dd526479b8dbc5b9bbc1
James Aworo <james.aworo@outlook.com>
2023-11-24 19:25:16 +0100

    update(crm-web): add  new components in pipeline module declaration

--- crm-web/src/app/pages/pipeline/pipeline.module.ts
+++ crm-web/src/app/pages/pipeline/pipeline.module.ts
@@ -1,6 +1,9 @@
 import {NgModule, Type} from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
+import {PipelineDataRendererComponent} from './_components/pipeline-data-renderer/pipeline-data-renderer.component';
 import {PipelineFilterComponent} from './_components/pipeline-filter/pipeline-filter.component';
+import {PipelineGridComponent} from './_components/pipeline-grid/pipeline-grid.component';
+import {PipelineListComponent} from './_components/pipeline-list/pipeline-list.component';
 import {PipelineTableComponent} from './_components/pipeline-table/pipeline-table.component';
 
 import {PipelineRoutingModule} from './pipeline-routing.module';
@@ -12,6 +15,9 @@ const COMPONENTS: Array<Type<any>> = [
     PipelinesComponent,
     PipelineFilterComponent,
     PipelineTableComponent,
+    PipelineDataRendererComponent,
+    PipelineGridComponent,
+    PipelineListComponent,
 ];
 
 @NgModule({
