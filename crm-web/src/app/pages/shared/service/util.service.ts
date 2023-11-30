919d5edd9d92e7b3d9da0d1d1f48f164c6de5f59
James Aworo <james.aworo@outlook.com>
2023-11-30 04:25:20 +0100

    update(crm-web): add application wide util functions

---
+++ crm-web/src/app/pages/shared/service/util.service.ts
@@ -0,0 +1,60 @@
+import {Injectable} from '@angular/core';
+import {NbToastrService} from '@nebular/theme';
+import {NbComponentOrCustomStatus} from '@nebular/theme/components/component-status';
+import {firstValueFrom, Observable} from 'rxjs';
+import {IOption} from '../data/shared.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UtilService {
+
+    constructor(private toast: NbToastrService) {
+    }
+
+    mapToOptionList<T extends Record<string, string>>(input: T): IOption[] {
+        return Object.keys(input)
+            .filter(key => isNaN(Number(key)))
+            .map(key => ({
+                label: key.replace(/_/g, ' '),
+                value: input[key]
+            }));
+    }
+
+    public async handleRequest<T>(
+        arg: Observable<T>,
+        message?: { success?: string; error?: string, title?: string }
+    ): Promise<T> {
+
+        try {
+            const value: any = await firstValueFrom(arg);
+
+            if (value && value?.ok) {
+                this.showToast({message: value.body.message, title: message?.title ?? 'Success!', status: 'success'});
+            } else {
+                this.showToast({message: value.body.message, title: message?.title ?? 'Failed!', status: 'danger'});
+            }
+            return value;
+        } catch (err: any) {
+            const error = err.error;
+
+            if (err && error) {
+                this.showToast({message: error.errors, title: error.message, status: 'danger'});
+            }
+            // send error to logger service
+            return Promise.resolve(err);
+        }
+    }
+
+    public showToast(args: { status: NbComponentOrCustomStatus, message?: string, title?: string, }): void {
+        let {message, title} = args;
+
+        if (args.status === 'danger') {
+            message = message ?? 'Something has gone wrong';
+            title = title ?? 'An error occurred!';
+        }
+
+        this.toast.show(message, title,
+            {duration: 10000, preventDuplicates: true, status: args.status});
+    }
+}
